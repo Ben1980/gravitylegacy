03cf25738c833dae40ba527f6e577f2808552149
Benjamin Mahr <ben.mahr@gmail.com>
2018-07-13 14:35:57 +0200

    Validation Method added and ParticleBuilder wrapped with unique_ptr

--- utilsTest/tst_utilstest.cpp
+++ utilsTest/tst_utilstest.cpp
@@ -1,5 +1,6 @@
 #include <QString>
 #include <QtTest>
+#include <memory>
 
 #include "particles.h"
 #include "particlebuilder.h"
@@ -16,10 +17,20 @@ private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
     void testParticleBuilderWithTwoParticles();
+
+private:
+    const double EPSILON = 1e-15;
+
+    bool resultIsValid(double expected, double result, double epsilon) const;
 };
 
 UtilsTest::UtilsTest()
 {
+}
+
+bool UtilsTest::resultIsValid(double expected, double result, double epsilon) const
+{
+    return fabs(result/expected - 1) <= epsilon;
 }
 
 void UtilsTest::initTestCase()
@@ -32,16 +43,16 @@ void UtilsTest::cleanupTestCase()
 
 void UtilsTest::testParticleBuilderWithTwoParticles()
 {
-    ParticleBuilder *builder = new ParticleBuilderImpl();
+    std::unique_ptr<ParticleBuilder> builder = std::make_unique<ParticleBuilderImpl>();
     Particles particles;
     builder->setPosition(-0.5, -0.5)->addParticle(particles);
     builder->setPosition(0.5, 0.5)->addParticle(particles);
 
     QVERIFY2(particles.getNumberOfParticles() == 2, "Number of particles generated is not two!");
-    QVERIFY2(particles.xPosition[0] == -0.5, "X Position of Particle 1 is wrong!");
-    QVERIFY2(particles.yPosition[0] == -0.5, "Y Position of Particle 1 is wrong!");
-    QVERIFY2(particles.xPosition[1] == 0.5, "X Position of Particle 2 is wrong!");
-    QVERIFY2(particles.yPosition[1] == 0.5, "Y Position of Particle 2 is wrong!");
+    QVERIFY2(resultIsValid(-0.5, particles.xPosition[0], EPSILON), "X Position of Particle 1 is wrong!");
+    QVERIFY2(resultIsValid(-0.5, particles.yPosition[0], EPSILON), "Y Position of Particle 1 is wrong!");
+    QVERIFY2(resultIsValid(0.5, particles.xPosition[1], EPSILON), "X Position of Particle 2 is wrong!");
+    QVERIFY2(resultIsValid(0.5, particles.yPosition[1], EPSILON), "Y Position of Particle 2 is wrong!");
 }
 
 QTEST_APPLESS_MAIN(UtilsTest)
