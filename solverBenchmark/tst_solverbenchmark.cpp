904ce7b68670ba19b6c2a7531b524d9cdffadcff
Benjamin Mahr <ben.mahr@gmail.com>
2018-07-16 12:56:49 +0200

    Simple benchmark test with 10000 particles added. Takes 18.9s

--- solverBenchmark/tst_solverbenchmark.cpp
+++ solverBenchmark/tst_solverbenchmark.cpp
@@ -1,5 +1,13 @@
 #include <QString>
 #include <QtTest>
+#include <memory>
+#include <random>
+
+#include "particlebuilder.h"
+#include "particlebuilderimpl.h"
+#include "particles.h"
+#include "solver.h"
+#include "euler.h"
 
 class SolverBenchmark : public QObject
 {
@@ -11,11 +19,19 @@ public:
 private Q_SLOTS:
     void initTestCase();
     void cleanupTestCase();
-    void testCase1();
+    void testPerformanceWith10000Particles();
+
+private:
+    constexpr static const double DELTA_T = 1e-1;
+
+    std::unique_ptr<ParticleBuilder> builder;
+    std::unique_ptr<Solver> solver;
 };
 
 SolverBenchmark::SolverBenchmark()
 {
+    builder = std::make_unique<ParticleBuilderImpl>();
+    solver = std::make_unique<Euler>();
 }
 
 void SolverBenchmark::initTestCase()
@@ -26,10 +42,23 @@ void SolverBenchmark::cleanupTestCase()
 {
 }
 
-void SolverBenchmark::testCase1()
+void SolverBenchmark::testPerformanceWith10000Particles()
 {
-    //QFETCH(QString, data);
+    std::default_random_engine generator;
+    std::uniform_real_distribution<double> posDistribution(0, 1000);
+    std::uniform_real_distribution<double> massDistribution(1e5, 1e20);
+
+    Particles particles;
+    for(size_t particle = 0; particle < 10000; particle++) {
+        const double posX = posDistribution(generator);
+        const double posY = posDistribution(generator);
+        const double mass = massDistribution(generator);
+
+        builder->setPosition(posX, posY)->setMass(mass)->addParticle(particles);
+    }
+
     QBENCHMARK {
+        const Particles solution = solver->solve(particles, DELTA_T);
     }
 }
 
